86501b071df2dce34329a3a904b2086239c10ae7
Michel Cuypers <mcu@tectumgroup.be>
2022-02-10 18:30:54 +0100

    Class created

---
+++ trackerLibrary/PrizeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trackerLibrary
+{
+    public class PrizeModel
+    {
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+
+    }
+}
