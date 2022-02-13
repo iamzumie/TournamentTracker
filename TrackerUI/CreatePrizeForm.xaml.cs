dbef7230ce6d100bbfd7b404d921e2ee4d249125
Michel Cuypers <mcu@tectumgroup.be>
2022-02-13 21:04:30 +0100

    createForm valid

--- TrackerUI/CreatePrizeForm.xaml.cs
+++ TrackerUI/CreatePrizeForm.xaml.cs
@@ -28,5 +28,44 @@ namespace TrackerUI
         {
 
         }
+
+        private bool ValidateForm()
+        {
+            bool output = true;
+            int placeNumber = 0;
+            
+            bool placeNumberValidNumber = int.TryParse(txtPlaceNumber.Text, out placeNumber);
+
+            // If it's a valid number
+            if (placeNumberValidNumber == false)
+            {
+                output = false;
+            }
+
+            // If number isn't 0
+            if (placeNumber < 1)
+            {
+                output = false;
+            }
+
+            if (txtPlaceName.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeAmount = 0;
+            int prizePercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(txtPlaceAmount.Text, out prizeAmount);
+            bool prizePercentageValid = int.TryParse(txtPrizePercentage.Text, out prizePercentage);
+
+            // If prizeAmount or prizePercentage is valid
+            if (prizeAmountValid == false || prizePercentageValid == false)
+            {
+
+            }
+
+            return output;
+        }
     }
 }
